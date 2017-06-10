e72706752eb4d4152294b84701713e8dfd25e667
Nate Norberg <nate.norberg@goquiq.com>
2017-06-10 15:32:24 -0600

    Got rid of prop-types errors (and upgraded to enzyme)

--- src/StaggerSpec.js
+++ src/StaggerSpec.js
@@ -1,91 +1,91 @@
 import Stagger from './Stagger';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+import {mount} from 'enzyme';
 
-describe('Stagger component', function() {
-  beforeEach(function() {
+describe('Stagger component', () => {
+  let testProps;
+  let wrapper;
+
+  beforeEach(() => {
     spyOn(window, 'setTimeout');
     const children = [<div>One</div>, <div>Two</div>];
-    this.testProps = {
+    testProps = {
       transition: 'zoom',
       delay: 100,
       initialDelay: 10,
       className: 'testContainer',
       children
     };
-    this.component = TestUtils.renderIntoDocument(<Stagger {...this.testProps} />);
-    this.node = ReactDOM.findDOMNode(this.component);
+
+    wrapper = mount(<Stagger {...testProps}/>);
   });
 
-  describe('initial state', function() {
-    it('sets childrenAdded to empty array and animationDone to false', function() {
-      expect(this.component.state.childrenAdded).toEqual([]);
-      expect(this.component.state.animationDone).toBe(false);
-    });
-  });
-
-  describe('componentWillMount', function() {
-    beforeEach(function() {
-      this.component.componentWillMount();
-    });
-
-    it('sets itemsAdded to 0', function() {
-      expect(this.component.itemsAdded).toBe(0);
-    });
-
-    it('queues up the first animation', function() {
-      expect(window.setTimeout).toHaveBeenCalledWith(this.component.addItem, 10);
+  describe('initial state', () => {
+    it('sets childrenAdded to empty array and animationDone to false', () => {
+      expect(wrapper.state('childrenAdded')).toEqual([]);
+      expect(wrapper.state('animationDone')).toBe(false);
     });
   });
 
-  describe('addItem', function() {
-    describe('when there are still items left', function() {
-      beforeEach(function() {
-        this.component.addItem();
+  describe('componentWillMount', () => {
+    beforeEach(() => {
+      wrapper.instance().componentWillMount();
+    });
+
+    it('sets itemsAdded to 0', () => {
+      expect(wrapper.instance().itemsAdded).toBe(0);
+    });
+
+    it('queues up the first animation', () => {
+      expect(window.setTimeout).toHaveBeenCalledWith(wrapper.instance().addItem, 10);
+    });
+  });
+
+  describe('addItem', () => {
+    describe('when there are still items left', () => {
+      beforeEach(() => {
+        wrapper.instance().addItem();
       });
 
-      it('adds the next item to childrenAdded', function() {
-        expect(this.component.state.childrenAdded).toEqual([<div>One</div>]);
+      it('adds the next item to childrenAdded', () => {
+        expect(wrapper.state('childrenAdded')).toEqual([<div>One</div>]);
       });
 
-      it('increments itemsAdded', function() {
-        expect(this.component.itemsAdded).toBe(1);
+      it('increments itemsAdded', () => {
+        expect(wrapper.instance().itemsAdded).toBe(1);
       });
 
-      it('sets the timer', function() {
-        expect(window.setTimeout).toHaveBeenCalledWith(this.component.addItem, 100);
+      it('sets the timer', () => {
+        expect(window.setTimeout).toHaveBeenCalledWith(wrapper.instance().addItem, 100);
       });
     });
 
-    describe('when there are no items left', function() {
-      it('sets animationDone to true', function() {
-        this.component.itemsAdded = 2;
-        this.component.addItem();
-        expect(this.component.state.animationDone).toBe(true);
+    describe('when there are no items left', () => {
+      it('sets animationDone to true', () => {
+        wrapper.instance().itemsAdded = 2;
+        wrapper.instance().addItem();
+        expect(wrapper.state('animationDone')).toBe(true);
       });
     });
   });
 
-  describe('render', function() {
-    it('renders the container', function() {
-      expect(this.node.className).toBe('testContainer');
+  describe('render', () => {
+    it('renders the container', () => {
+      expect(wrapper.hasClass('testContainer')).toBe(true);
     });
 
-    describe('when none of the children have been added', function() {
-      it('adds the enter class to all the children', function() {
-        expect(this.node.children[0].className).toBe('zoom-enter');
-        expect(this.node.children[1].className).toBe('zoom-enter');
+    describe('when none of the children have been added', () => {
+      it('adds the enter class to all the children', () => {
+        expect(wrapper.children().every('.zoom-enter')).toBe(true);
       });
     });
 
-    describe('when one of the children has been added', function() {
-      it('adds the enter and active classes', function() {
-        this.component.addItem();
-        expect(this.node.children[0].className).toBe('zoom-enter zoom-enter-active');
-        expect(this.node.children[1].className).toBe('zoom-enter');
+    describe('when one of the children has been added', () => {
+      it('adds the enter and active classes', () => {
+        wrapper.instance().addItem();
+        expect(wrapper.children().at(0).prop('className')).toBe('zoom-enter zoom-enter-active');
+        expect(wrapper.children().at(1).prop('className')).toBe('zoom-enter');
       });
     });
   });
 });
-
